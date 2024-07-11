af4b83c9d47914b6ccbb594fbfad41bf8c9a455c
Willyan Costa <willyancr@gmail.com>
2024-07-11 17:06:17 -0300

    Add sidebar component with social media links
    Create a new sidebar component with links to Twitter, Instagram, and GitHub profiles using respective icons.

---
+++ src/app/components/sidebar/page.tsx
@@ -0,0 +1,31 @@
+import { FaGithub, FaInstagram, FaXTwitter } from 'react-icons/fa6';
+
+export default function SideBar() {
+  return (
+    <div className="flex h-[740px]">
+      <aside className="flex flex-col-reverse gap-6 min-w-16 sm:flex pl-3">
+        <a
+          href="https://x.com/WillyanCosta"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaXTwitter />
+        </a>
+        <a
+          href="https://www.instagram.com/willyan.cr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://github.com/willyancr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </aside>
+    </div>
+  );
+}
