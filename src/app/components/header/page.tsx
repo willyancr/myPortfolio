33d19d35093b24869cfe235babc3feca00758c0f
Willyan Costa <willyancr@gmail.com>
2024-07-11 17:06:33 -0300

    update : Refactor header component layout and add navigation links
    Update the header component layout to include navigation links using the Link component from Next.js. This improves the user experience and enhances navigation within the application.

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link';
+
 export default function HeaderPage() {
   return (
-    <nav className="container">
-      <h1>W</h1>
-    </nav>
+    <header className="py-6 text-gray-300">
+      <nav className="container flex items-center justify-between">
+        <div>
+          <Link href="/">
+            <h1 className="text-5xl font-monoton ">W</h1>
+          </Link>
+        </div>
+        <div className="flex items-center gap-10 font-poppins ">
+          <Link href="/sobre" className="hover:text-gray-100 transition">
+            <span className="text-green-400">#</span>Sobre
+          </Link>
+          <Link href="/projetos" className="hover:text-gray-100 transition">
+            <span className="text-green-400">#</span>Projetos
+          </Link>
+          <Link href="/contato" className="hover:text-gray-100 transition">
+            <span className="text-green-400">#</span>Contatos
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
