bc96b0984784324a51e624e46d0b6bfc0f7393a2
Willyan Costa <willyancr@gmail.com>
2024-07-11 15:02:08 -0300

    create new project

---
+++ src/app/components/header/page.tsx
@@ -0,0 +1,7 @@
+export default function HeaderPage() {
+  return (
+    <nav className="container">
+      <h1>W</h1>
+    </nav>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['var(--font-inter)', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/hero-bg.png')",
+      },
+      fontFamily: {
+        mono: ['var(--font-plex-mono)', 'monospace'],
+      },
+      boxShadow: {
+        button: '0px 0px 68px 7px rgba(5, 150, 105, 0.4)',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
