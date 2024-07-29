7031c147d3a59fac3964f7aa5383644199359077
Willyan Costa <willyancr@gmail.com>
2024-07-28 22:25:27 -0300

    update 2 files and create 1 file

--- src/app/(myportfolio)/projetos/page.tsx
+++ src/app/(myportfolio)/projetos/page.tsx
@@ -1,27 +1,10 @@
+import Cards from '@/app/components/cards-projetos';
+
 export default function ProjetosPage() {
   return (
     <div className="container space-y-8">
       <h1 className="font-mono">#Projetos</h1>
-      <div className="flex flex-wrap gap-6 items-center justify-center">
-        <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl">
-          <p>teste</p>
-        </div>
-        <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl">
-          <p>teste</p>
-        </div>
-        <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl">
-          <p>teste</p>
-        </div>
-        <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl">
-          <p>teste</p>
-        </div>
-        <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl">
-          <p>teste</p>
-        </div>
-        <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl">
-          <p>teste</p>
-        </div>
-      </div>
+      <Cards />
     </div>
   );
 }
---
+++ src/app/components/cards-projetos.tsx
@@ -0,0 +1,24 @@
+export default function Cards() {
+  return (
+    <div className="flex flex-wrap gap-6 items-center justify-center">
+    <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl shadowCards transition duration-500">
+      <p>teste</p>
+    </div>
+    <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl shadowCards transition duration-500">
+      <p>teste</p>
+    </div>
+    <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl shadowCards transition duration-500">
+      <p>teste</p>
+    </div>
+    <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl shadowCards transition duration-500">
+      <p>teste</p>
+    </div>
+    <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl shadowCards transition duration-500">
+      <p>teste</p>
+    </div>
+    <div className="w-[287px] h-[300px] bg-cards rounded-3xl shadow-xl shadowCards transition duration-500">
+      <p>teste</p>
+    </div>
+  </div>
+  );
+}
