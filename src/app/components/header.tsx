e44ddd1784bf8543cba0a46b76aa6c93ab7bd2a6
Willyan Costa <willyancr@gmail.com>
2024-08-17 20:34:52 -0300

    update: Refactor header component styles and links
    Update header component styles and link destinations for better layout and navigation.

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,17 +3,15 @@ import Redirect from "./redirect";
 
 export default function HeaderPage() {
   return (
-    <header>
-      <nav className="container fixed top-0 z-50 flex items-center justify-between bg-[#101A21] py-6 text-gray-300">
-        <div>
-          <Link href="/">
-            <h1 className="font-monoton text-5xl">W</h1>
-          </Link>
-        </div>
-        <div className="flex items-center gap-2 pr-10 font-mono text-sm sm:gap-6 sm:text-base">
-          <Redirect href="/sobre">Sobre</Redirect>
-          <Redirect href="/projetos">Projetos</Redirect>
-          <Redirect href="/contato">Contato</Redirect>
+    <header className="fixed top-0 z-50 block w-full bg-[#101A21] py-6 text-gray-300">
+      <nav className="container flex items-center justify-between gap-2">
+        <Link href="#initial">
+          <h1 className="font-monoton text-5xl">W</h1>
+        </Link>
+        <div className="flex items-center font-mono text-sm sm:gap-6 sm:text-base">
+          <Redirect href="#sobre">Sobre</Redirect>
+          <Redirect href="#projetos">Projetos</Redirect>
+          <Redirect href="#contato">Contato</Redirect>
         </div>
       </nav>
     </header>
