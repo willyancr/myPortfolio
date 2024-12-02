21d8a364c91c4eb986f3cfa8269335296cdb599c
Willyan Costa <willyancr@gmail.com>
2024-12-02 20:13:43 -0300

    chore: update imports

--- src/app/(myportfolio)/(hero)/page.tsx
+++ src/app/(myportfolio)/(hero)/page.tsx
@@ -1,8 +1,8 @@
-import HeaderPage from "@/app/components/header";
-import Contact from "../../components/sections/contato/page";
 import InitialSection from "../../components/sections/initial/page";
 import ProjectPage from "../../components/sections/projeto/page";
+import Contact from "../../components/sections/contato/page";
 import About from "../../components/sections/sobre/page";
+import HeaderPage from "@/app/components/header";
 
 export default async function HomePage() {
   return (
